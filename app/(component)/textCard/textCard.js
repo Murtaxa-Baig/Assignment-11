99f204c63bd829b9def7c04883a9d7e37e30d9df
Murtaxa-Baig <darvaish49@gmail.com>
2023-09-23 19:50:28 +0500

    Adding CMS

--- app/(component)/textCard/textCard.js
+++ app/(component)/textCard/textCard.js
@@ -5,9 +5,7 @@ export default function TextCard(proops) {
         <>
             <div className="course-col">
                 <h3>{proops.title}</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolor corporis, commodi nihil quas
-                    soluta labore quisquam impedi distinctio explicabo aut minima quos pariatur unde aliquam earum
-                    laborum velit non.</p>
+                <p>{proops.desc}</p>
             </div>
 
         </>
--- app/page.js
+++ app/page.js
@@ -1,21 +1,58 @@
+
+const fetchCourse = async () => {
+  let response = await client.getEntries({ content_type: "courses" })
+  
+  const courses = response?.items?.map((item) => {
+    return {
+      title: item?.fields?.courseName,
+      desc: item?.fields?.description
+    }
+  })
+  return courses
+}
+
+const fetchImg = async () => {
+  let response = await client.getEntries({ content_type: "tour" })
+  const images = response?.items?.map((items) => {
+    // console.log(items.fields.img.fields.file.url)
+    return {
+      img: items.fields.img.fields.file.url
+    }
+  })
+  return images
+}
+const fetchbestCourses = async () => {
+  let response = await client.getEntries({ content_type: "BestCourseCard" })
+  const bestCourses = response?.items?.map((items) => {
+    return {
+      title: items?.fields?.title,
+      desc: items?.fields?.Description,
+      img:items?.fields
+    }
+  })
+  return bestCourses
+}
+
 import Image from 'next/image'
-import styles from './page.module.css'
 import Navbar from './(component)/navbar/navbar'
 import Button from './(component)/button/button'
 import TextCard from './(component)/textCard/textCard'
-import ImCard from './(component)/imgCard/imgCard'
 import ImgCard from './(component)/imgCard/imgCard'
 import ImgTextCard from './(component)/imgTextCard/imgTextCard'
 import Link from 'next/link'
 import ImgCard1 from "@/public/img/libary.png"
 import ImgCard2 from "@/public/img/playground.png"
 import ImgCard3 from "@/public/img/food.png"
-import Homeimg1 from "@/public/img/Campus1.png"
-import Homeimg2 from "@/public/img/Campus2.png"
-import Homeimg3 from "@/public/img/Campus3.png"
+import client from '@/lib/contentful'
 
-export default function Home() {
- 
+export default async function Home() {
+  const course = await fetchCourse();
+  const image = await fetchImg();
+  const bestcourse = await fetchbestCourses();
+  console.log(bestcourse)
+
+
+
   return (
     <>
       <section className="header">
@@ -27,7 +64,7 @@ export default function Home() {
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit consequuntur corrupti sapiente aut porro
             <br /> esse blanditiis in quae perspiciatis quo.
           </p>
-          <Button title="Visit Us To Know More" CN="hero_btn"/>
+          <Button title="Visit Us To Know More" CN="hero_btn" />
         </div>
       </section>
       <section className="course">
@@ -38,10 +75,15 @@ export default function Home() {
         <h1>EXPLORE OUR 60+ <br /> MAJOR PROGRAMS</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
 
+
         <div className="row">
-          <TextCard title="Undergraduate Programs" />
-          <TextCard title="Graduate Programs" />
-          <TextCard title="Adult Learning & Degree Completion" />
+          {
+            course.map((items) => {
+              return (
+                <TextCard title={items.title} desc={items.desc} />
+              )
+            })
+          }
         </div>
       </section>
       <section className="campus">
@@ -53,39 +95,25 @@ export default function Home() {
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
 
         <div className="row">
-          <div className="campus-col">
-            <Image
-                    src={Homeimg1}
-                    width={400}
-                    height={250}
-                    alt="Picture of the author"
-                />
-            <div className="layer">
-              <h3>FAISALABAD</h3>
-            </div>
-          </div>
-          <div className="campus-col">
-            <Image
-                    src={Homeimg2}
-                    width={400}
-                    height={250}
-                    alt="Picture of the author"
-                />
-            <div className="layer">
-              <h3>LAHORE</h3>
-            </div>
-          </div>
-          <div className="campus-col">
-            <Image
-                    src={Homeimg3}
-                    width={400}
-                    height={250}
-                    alt="Picture of the author"
-                />
-            <div className="layer">
-              <h3>ISLAMABAD</h3>
-            </div>
-          </div>
+          {
+            image.map((items) => {
+              return (
+                <>
+                  <div className="campus-col">
+                    <img
+                      src={`https:${items.img}`}
+                      width={400}
+                      height={250}
+                      alt="Picture of the author"
+                    />
+                    <div className="layer">
+                      <h3>FAISALABAD</h3>
+                    </div>
+                  </div>
+                </>
+              )
+            })
+          }
         </div>
       </section>
 
@@ -100,7 +128,7 @@ export default function Home() {
         <div className="row">
           <ImgCard title="Best Libary" img={ImgCard1} />
           <ImgCard title="Athletics" img={ImgCard2} />
-          <ImgCard title="Tasty and Healthy Food" img={ImgCard3}/>
+          <ImgCard title="Tasty and Healthy Food" img={ImgCard3} />
         </div>
       </section>
 
@@ -120,7 +148,7 @@ export default function Home() {
 
       <section className="cta">
         <h1>GET READY FOR A BRIGHT FUTURE</h1>
-        <Link href="/contact"><Button title="Contact US" CN="hero_btn"/></Link>
+        <Link href="/contact"><Button title="Contact US" CN="hero_btn" /></Link>
       </section>
 
 
